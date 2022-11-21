233a6c546f224f0287368c470ffa34ec4ba0b0f0
thiago <thiago3399@hotmail.com>
2022-11-21 10:56:07 -0300

    create main page and add some functions

---
+++ src/components/addPokemonField/Style.js
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+
+    display: flex;
+    width: 100%;
+    height: 100%;
+    justify-content: center;
+    align-items: center;
+`;
+
+export const PokemonInput = styled.input`
+    width: 70%;
+    height: 20%;
+    display: flex;
+    border: 2px solid #000000;
+    border-radius: 10px;
+    font-size: 100%;
+    text-indent: 10px;
+    box-sizing: border-box;
+    outline: 0;
+`
---
+++ src/components/pokemonCard/style.js
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+export const EmptyCard = styled.div`
+    width: 15%;
+    height: 100%;
+    background-color: #000;
+    border-radius: 10px;
+    align-items: center;
+    justify-content: center;
+    margin-left: 1.5%;
+    text-align: center;
+`;
+
+export const CardNumber = styled.p`
+
+    font-weight: bolder;
+    color: #FFF;
+    font-size: 100px;
+`
